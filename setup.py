6027819e21049692979df1fdb0d67b09282070b3
Srusti H S <srusti.edcults@gmail.com>
2024-05-26 09:56:43 +0530

    file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 ## This file is For installing local package in virtual environment
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='srusti hs',
+    author_email='srustihs17@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
